f18adf5ff8488eeedee994d0d3b9fbde79776915
Francis Santerre <francis.santerre@gmail.com>
2017-03-26 13:32:22 -0400

    Update application name

--- app/resources/assets/js/components/Search.vue
+++ app/resources/assets/js/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="search">
-        <h1 class="titre">Nom du projet</h1>
+        <h1 class="titre">Le pointeur</h1>
         <p class="headline">Ce que vous avez entendu en ondes</p>
         <input class="search-input" type="text" v-model="query"  aria-label="Recherche" title="Recherche" />
         <button type="submit" class="search-btn" @click="search()">Rechercher <i class="fa fa-refresh fa-spin" v-show="searching"></i></button>
